2c905fa9baf48533201af87933cd9e549392b0b9
Pedro Alves <pta2002@pta2002.com>
2022-10-25 08:40:55 +0100

    otimização 8: desconstruir cluster

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ scp -r src Makefile includes "$1"@search7edu2.di.uminho.pt:~/cp
 ssh "$1"@s7edu2.di.uminho.pt "mkdir -p ~/cp && cd ~/cp;
 make clean;
 make k_means;
-srun --partition=cpar perf stat /home/$1/cp/bin/k_means"
+srun --partition=cpar perf stat -e L1-dcache-load-misses /home/$1/cp/bin/k_means"
--- src/k_means.c
+++ src/k_means.c
@@ -11,8 +11,12 @@
 int k, n_samples, seed;
 coordinate *samples;
 int *cluster_indices;
-cluster *clusters;
+// cluster *clusters;
 coordinate *centroids;
+
+float *cluster_x;
+float *cluster_y;
+int *cluster_size;
 
 float dist(coordinate a, coordinate b) {
   return (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y);
@@ -32,33 +36,35 @@ void init() {
   }
 
   // Initialize clusters
-  clusters = calloc(k, sizeof(cluster));
+  cluster_size = calloc(k, sizeof(int));
+  cluster_x = calloc(k, sizeof(float));
+  cluster_y = calloc(k, sizeof(float));
   centroids = calloc(k, sizeof(coordinate));
   for (int i = 0; i < k; i++) {
     cluster_indices[i] = i;
     // clusters[i].centroid.cluster_index = i;
-    clusters[i].size = 1;
+    cluster_size[i] = 1;
   }
 }
 
 void calc_centroids() {
   for (int i = 0; i < k; i++) {
-    clusters[i].sum_x = 0;
-    clusters[i].sum_y = 0;
-    clusters[i].size = 0;
+    cluster_x[i] = 0;
+    cluster_y[i] = 0;
+    cluster_size[i] = 0;
   }
 
   for (int i = 0; i < n_samples; i++) {
     coordinate *c = &samples[i];
-    clusters[cluster_indices[i]].sum_x += c->x;
-    clusters[cluster_indices[i]].sum_y += c->y;
-    clusters[cluster_indices[i]].size++;
+    cluster_x[cluster_indices[i]] += c->x;
+    cluster_y[cluster_indices[i]] += c->y;
+    cluster_size[cluster_indices[i]]++;
   }
 
   // Calculate centroids
   for (int i = 0; i < k; i++) {
-    centroids[i].x = clusters[i].sum_x / clusters[i].size;
-    centroids[i].y = clusters[i].sum_y / clusters[i].size;
+    centroids[i].x = cluster_x[i] / cluster_size[i];
+    centroids[i].y = cluster_y[i] / cluster_size[i];
   }
 
   //   for (int j = 0; j < clusters[i].size; j++) {
@@ -73,11 +79,6 @@ void calc_centroids() {
 bool distribute_elements() {
   bool changed = false;
 
-  // Reset cluster sizes;
-  for (int i = 0; i < k; i++) {
-    clusters[i].size = 0;
-  }
-
   for (int i = 0; i < n_samples; i++) {
     // Find nearest cluster
     int cluster_index = 0;
@@ -89,6 +90,14 @@ bool distribute_elements() {
     //   distances[j] = dist(samples[i], centroids[j]);
     // }
 
+    // float min = distances[0];
+    // for (int j = 1; j < k; j++) {
+    //   if (distances[j] < min) {
+    //     cluster_index = j;
+    //     min = distances[j];
+    //   }
+    // }
+
     float min = dist(samples[i], centroids[0]);
     for (int j = 0; j < k; j++) {
       float d = dist(samples[i], centroids[j]);
@@ -97,14 +106,6 @@ bool distribute_elements() {
         min = d;
       }
     }
-
-    // float min = distances[0];
-    // for (int j = 1; j < k; j++) {
-    //   if (distances[j] < min) {
-    //     cluster_index = j;
-    //     min = distances[j];
-    //   }
-    // }
 
     // Update data
     if (cluster_index != cluster_indices[i]) {
@@ -133,9 +134,8 @@ int main(int argc, char **argv) {
 
   printf("N = %d, K = %d\n", n_samples, k);
   for (int i = 0; i < k; i++) {
-    cluster c = clusters[i];
     printf("Center: (%f, %f) : Size: %d\n", centroids[i].x, centroids[i].y,
-           c.size);
+           cluster_size[i]);
   }
   printf("Iterations: %d\n", iterations);
 }
