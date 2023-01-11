58fe3fa921ddf2aaf336e9be68f4823ff0b7a281
SugaryLump <sugarcanegaming6000@gmail.com>
2023-01-11 15:14:11 +0000

    Shared mem + GPU centroid sums

--- src/k_means.cu
+++ src/k_means.cu
@@ -56,57 +56,27 @@ void init(float *d_sample_x, float *d_sample_y, int *d_cluster_indices,
   checkCUDAError("Init error");
 }
 
-void calc_centroids(int *d_cluster_indices,
-                    float *d_centroids_x, float *d_centroids_y)
+void calc_centroids(float *d_centroids_x, float *d_centroids_y,
+                    float *d_centroid_sum_x, float *d_centroid_sum_y, 
+                    int *d_cluster_count)
 {
-  // New host array of cluster indices because they´ve been calculated
-  // on device
-  int *h_cluster_indices = (int*)malloc(sizeof(int) * N);
-  // Grab new cluster indices from device
-  cudaMemcpy(h_cluster_indices, d_cluster_indices, N * 4, cudaMemcpyDeviceToHost);
-  // New host array for centroid  coordinates because we need to calculate
-  // it on host before sending it over to device
-  float h_centroids_x[K], h_centroids_y[K];
-  // Local cluster coordinates sum
-  float cluster_x[K], cluster_y[K];
-  // Local array for parallelization of middle loop with openmp (reduce needs
-  // local array)
-  int p_cluster_size[K];
+    float h_centroids_x[K];
+    float h_centroids_y[K];
+    float h_centroid_sum_x[K], h_centroid_sum_y[K];
+    int h_cluster_count[K];
+    cudaMemcpy(h_centroid_sum_x, d_centroid_sum_x, K * 4, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_centroid_sum_y, d_centroid_sum_y, K * 4, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_cluster_count, d_cluster_count, K * 4, cudaMemcpyDeviceToHost);
 
-  // Init cluster sum/size arrays
-#pragma omp smd
-  for (int i = 0; i < K; i++)
-  {
-    cluster_x[i] = 0;
-    cluster_y[i] = 0;
-    p_cluster_size[i] = 0;
-  }
+    for (int i = 0; i < K; i++) {
+      h_centroids_x[i] = h_centroid_sum_x[i] / h_cluster_count[i];
+      h_centroids_y[i] = h_centroid_sum_y[i] / h_cluster_count[i];
+      //printf("%d- %f, %f, %d\n", i, h_centroid_sum_x[i], h_centroid_sum_y[i], h_cluster_count[i]);
+    }
+    //printf("\n");
 
-  // Calculate cluster sums/size with openmp parallelization
-#pragma omp parallel for reduction(+ : cluster_x, cluster_y, p_cluster_size)
-  for (int i = 0; i < N; i++)
-  {
-    cluster_x[h_cluster_indices[i]] += h_sample_x[i];
-    cluster_y[h_cluster_indices[i]] += h_sample_y[i];
-    p_cluster_size[h_cluster_indices[i]]++;
-  }
-
-  // Calculate centroids
-  for (int i = 0; i < K; i++)
-  {
-    h_cluster_size[i] = p_cluster_size[i];
-    h_centroids_x[i] = cluster_x[i] / p_cluster_size[i];
-    h_centroids_y[i] = cluster_y[i] / p_cluster_size[i];
-    // printf("%d- size: %d, centroid %f, %f\n", i, h_cluster_size[i], h_centroids_x[i], h_centroids_y[i]);
-  }
-  // printf("\n");
-
-  // Send new centroids to device
-  cudaMemcpy(d_centroids_x, h_centroids_x, K * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_centroids_y, h_centroids_y, K * sizeof(float), cudaMemcpyHostToDevice);
-  checkCUDAError("Centroids copy error");
-
-  free(h_cluster_indices);
+    cudaMemcpy(d_centroids_x, h_centroids_x, K * 4, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_centroids_y, h_centroids_y, K * 4, cudaMemcpyHostToDevice);
 }
 
 // Used if SAMPLES_PER_THREAD > 1
@@ -114,40 +84,50 @@ __global__
 void distribute_elements_kernel(float *d_sample_x, float *d_sample_y,
                                 float *d_centroids_x, float *d_centroids_y,
                                 int *d_cluster_indices,
+                                int *d_cluster_count, float *d_centroids_sum_x,
+                                float *d_centroids_sum_y,
                                 bool *changed, int N, const int K, const int SperT)
 {
   int bid = threadIdx.x;
   int id = blockDim.x * blockIdx.x + bid;
 
-  //Shared memory array that divided as follows:
-  // {sample_0.x, sample0.y, sample1.x, etc..., centroid_0.x, centroid_0.y, etc...}
-  //extern __shared__ float shared_samples[];
-  //int cent_start = blockDim.x * SperT; //index of x coord of centroid 0
-  //
-  //for (int i = bid; i - bid < SperT &&  id + i - bid < N; i++) {
-  //  shared_samples[i * 2] = d_sample_x[id + i - bid];
-  //  shared_samples[i * 2 + 1] = d_sample_y[id + i - bid];
-  //}
-  //
-  //if (bid < K) {
-  //  shared_samples[cent_start + bid * 2] = d_centroids_x[bid];
-  //  shared_samples[cent_start + bid * 2 + 1] = d_centroids_y[bid];
-  //}
-  //
-  //__syncthreads();
+  // Shared memory array divided as follows:
+  // {sample_0.x, sample0.y, sample1.x, ..., centroid_0.x, centroid_0.y, ...,
+  //  new_centroid_0_x_sum, new_centroid_0_y_sum, cluster_0_count, ...}
+  // The second line of data is for new centroid calculation
+  extern __shared__ float shared_samples[];
+  int shared_ind = bid * SperT * 2; //index of x of first sample for this thread
+  int cent_start = blockDim.x * SperT * 2; //index of x coord of centroid 0
+  int new_start = cent_start + K * 2; //index of x coord of new centroid 0 sum
+  
+  for (int i = 0; i < SperT && id + i < N; i++) {
+    shared_samples[shared_ind + i * 2] = d_sample_x[id + i];
+    shared_samples[shared_ind + i * 2 + 1] = d_sample_y[id + i];
+  }
+  
+  if (bid < K) {
+    shared_samples[cent_start + bid * 2] = d_centroids_x[bid];
+    shared_samples[cent_start + bid * 2 + 1] = d_centroids_y[bid];
+    shared_samples[new_start + bid * 3] = 0;
+    shared_samples[new_start + bid * 3 + 1] = 0;
+    shared_samples[new_start + bid * 3 + 2] = 0;
+  }
+  
+  __syncthreads();
 
-  int start_i = id * SperT;
-  for (int i = id * SperT; i - start_i < SperT && i < N; i++)
+  for (int i = 0; i < SperT && id + i < N; i++)
   {
-    // Find nearest cluster
     int cluster_index = 0;
-
-    float min = dist(d_sample_x[i], d_sample_y[i],
-                     d_centroids_x[0], d_centroids_y[0]);
+    float min = dist(shared_samples[shared_ind + i * 2], shared_samples[shared_ind + i * 2 + 1],
+                     shared_samples[cent_start], shared_samples[cent_start + 1]);
+    //float min = dist(d_sample_x[id + i], d_sample_y[id + i],
+    //                 d_centroids_x[0], d_centroids_y[0]);
     for (int j = 1; j < K; j++)
     {
-      float d = dist(d_sample_x[i], d_sample_y[i],
-                     d_centroids_x[j], d_centroids_y[j]);
+      float d = dist(shared_samples[shared_ind + i * 2], shared_samples[shared_ind + i * 2 + 1],
+                     shared_samples[cent_start + j * 2], shared_samples[cent_start + j * 2 + 1]);
+      //float d = dist(d_sample_x[id + i], d_sample_y[id + i],
+      //               d_centroids_x[j], d_centroids_y[j]);
       if (d < min)
       {
         cluster_index = j;
@@ -156,11 +136,28 @@ void distribute_elements_kernel(float *d_sample_x, float *d_sample_y,
     }
 
     // Update data
-    if (cluster_index != d_cluster_indices[i])
+    if (cluster_index != d_cluster_indices[id + i])
     {
       *changed = true;
     }
-    d_cluster_indices[i] = cluster_index;
+    d_cluster_indices[id + i] = cluster_index;
+  }
+
+  __syncthreads();
+  // Sum centroids and cluster counts
+  if ((*changed) && bid == 0) {
+    for (int i = 0; i < SperT * blockDim.x && blockIdx.x * blockDim.x * SperT + i < N; i++) {
+      int cluster = d_cluster_indices[blockIdx.x * blockDim.x * SperT + i];
+      shared_samples[new_start + cluster * 3] += shared_samples[i * 2];
+      shared_samples[new_start + cluster * 3 + 1] += shared_samples[i * 2 + 1];
+      shared_samples[new_start + cluster * 3 + 2] += 1;
+    }
+
+    for  (int i = 0; i < K; i++) {
+      atomicAdd(&d_centroids_sum_x[i], shared_samples[new_start + i * 3]);
+      atomicAdd(&d_centroids_sum_y[i], shared_samples[new_start + i * 3 + 1]);
+      atomicAdd(&d_cluster_count[i], shared_samples[new_start + i * 3 + 2]);
+    }
   }
 }
 
@@ -183,12 +180,11 @@ int main(int argc, char **argv)
       }
     }
   }
-  // # Openmp prepping
-  omp_set_num_threads(64);
 
   // # Allocate memory on device
   float *d_sample_x, *d_sample_y, *d_centroids_x, *d_centroids_y;
-  int *d_cluster_indices;
+  float *d_centroid_sum_x, *d_centroid_sum_y;
+  int *d_cluster_indices, *d_cluster_count;
 
   int n_bytes = 4 * N;
   int k_bytes = 4 * K;
@@ -198,6 +194,12 @@ int main(int argc, char **argv)
   cudaMalloc((void **)&d_cluster_indices, n_bytes);
   cudaMalloc((void **)&d_centroids_x, k_bytes);
   cudaMalloc((void **)&d_centroids_y, k_bytes);
+  cudaMalloc((void **)&d_centroid_sum_x, k_bytes);
+  cudaMalloc((void **)&d_centroid_sum_y, k_bytes);
+  cudaMalloc((void **)&d_cluster_count, k_bytes);
+  cudaMemset(d_centroid_sum_x, 0, k_bytes);
+  cudaMemset(d_centroid_sum_y, 0, k_bytes);
+  cudaMemset(d_cluster_count, 0, k_bytes);
   checkCUDAError("Malloc error");
 
   // # Generate values and place in device memory
@@ -209,31 +211,49 @@ int main(int argc, char **argv)
   cudaMalloc((void **)&d_changed, sizeof(bool));
   cudaMemset(d_changed, 0, sizeof(bool));
   // ## First iteration sample distribution
-  int sh_bytes = SAMPLES_PER_THREAD * THREADS_PER_BLOCK * sizeof(float) * 2;
+  // ### sh_bytes is the amount of bytes of shared memory. Shared memory will 
+  // ### have all the block's respective samples, all the current centroid
+  // ### coordinates, and room for new centroid calculation at the end.
+  int sh_bytes = SAMPLES_PER_THREAD * THREADS_PER_BLOCK * sizeof(float) * 2 + K * 5 * sizeof(float);
   distribute_elements_kernel<<<N_BLOCKS, THREADS_PER_BLOCK, sh_bytes>>>(d_sample_x,
                                                                         d_sample_y,
                                                                         d_centroids_x,
                                                                         d_centroids_y,
                                                                         d_cluster_indices,
+                                                                        d_cluster_count,
+                                                                        d_centroid_sum_x,
+                                                                        d_centroid_sum_y,
                                                                         d_changed,
                                                                         N, K, SAMPLES_PER_THREAD);
   cudaDeviceSynchronize();
+  checkCUDAError("Device sync");
   // ## Get convergence flag from device
   cudaMemcpy(&h_changed, d_changed, sizeof(bool), cudaMemcpyDeviceToHost);
+  checkCUDAError("Getting flag");
   int iterations;
-  for (iterations = 0; h_changed && iterations < 21; iterations++)
+  for (iterations = 0; iterations < 21 && h_changed; iterations++)
   {
-    // ## Calculate centroids
-    calc_centroids(d_cluster_indices, d_centroids_x, d_centroids_y);
+    // ## Recalculate centroids
+    calc_centroids(d_centroids_x, d_centroids_y, d_centroid_sum_x, d_centroid_sum_y, d_cluster_count);
+
     // ## Reset 'changed' flags on device and host
     h_changed = false;
     cudaMemset(d_changed, 0, sizeof(bool));
+
+    // ## Reset accumulator arrays
+    cudaMemset(d_centroid_sum_x, 0, k_bytes);
+    cudaMemset(d_centroid_sum_y, 0, k_bytes);
+    cudaMemset(d_cluster_count, 0, k_bytes);
+
     // ## Redistribute elements
     distribute_elements_kernel<<<N_BLOCKS, THREADS_PER_BLOCK, sh_bytes>>>(d_sample_x,
                                                                           d_sample_y,
                                                                           d_centroids_x,
                                                                           d_centroids_y,
                                                                           d_cluster_indices,
+                                                                          d_cluster_count,
+                                                                          d_centroid_sum_x,
+                                                                          d_centroid_sum_y,
                                                                           d_changed,
                                                                           N, K, SAMPLES_PER_THREAD);
     cudaDeviceSynchronize();                                                                  
@@ -244,12 +264,14 @@ int main(int argc, char **argv)
   // # Print results
   printf("N = %d, K = %d\n", N, K);
   float h_centroids_x[K], h_centroids_y[K];
+  int h_cluster_count[K];
   cudaMemcpy(h_centroids_x, d_centroids_x, K * 4, cudaMemcpyDeviceToHost);
   cudaMemcpy(h_centroids_y, d_centroids_y, K * 4, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_cluster_count, d_cluster_count, K * 4, cudaMemcpyDeviceToHost);
   for (int i = 0; i < K; i++)
   {
     printf("Center: (%1.3f, %1.3f) : Size: %d\n", h_centroids_x[i],
-           h_centroids_y[i], h_cluster_size[i]);
+           h_centroids_y[i], h_cluster_count[i]);
   }
   printf("Iterations: %d\n", iterations);
 }
